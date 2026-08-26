6631f513e48e6b16f1dba3d9f782d22363e01eb5
RJ6776-AKJ <rj6776jha@gmail.com>
2026-08-26 20:43:29 +0530

    Added Switch Case Problem Number 6

--- src/switchCaseNumberProblemNumber6.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-public class switchCaseNumberProblemNumber6 {
-    static void main() {
-
-    }
-}
---
+++ src/switchCaseProblemNumber6.java
@@ -0,0 +1,5 @@
+public class switchCaseProblemNumber6 {
+    static void main() {
+
+    }
+}
