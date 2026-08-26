8a8c08fbbd96da4f90cf122f2861f9e256928d6d
RJ6776-AKJ <rj6776jha@gmail.com>
2026-08-26 23:04:27 +0530

    added switch Case Problem Number6

--- src/switchCaseProblemNumber6.java
+++ src/switchCaseProblemNumber6.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //------ BANK MENU ------
 //
 //1. Check Balance
@@ -8,7 +10,66 @@
 //
 //Enter your choice:
 public class switchCaseProblemNumber6 {
-    public static void main() {
+    public static void main(String[] args) {
+        int bal = 10000;
+        String pass= "1234";
+        Scanner sc = new Scanner(System.in);
+        int attempt = 0;
+        boolean loginSuccessful = false;
+        while (attempt < 3) {
+            System.out.println("Enter your Pin :- ");
+            String in = sc.nextLine();
+            if (in.equals(pass)) {
+                System.out.println("login authentication Successful");
+                break;
+            } else {
+                System.out.println("login authentication failed");
+                attempt++;
+                System.out.println("reamining attempt" + (3 - attempt));
+            }
+        }
+        int ch = 0;
+        while (ch < 5) {
+
+            System.out.println("Entering to Banking system");
+        System.out.println("------ BANK MENU ------\n" +
+                "1. Check Balance\n" +
+                "2. Deposit Money\n" +
+                "3. Withdraw Money\n" +
+                "4. Change PIN\n" +
+                "5. Exit");
+
+            System.out.println("Enter your choice");
+            ch = sc.nextInt();
+            switch (ch) {
+                case 1:
+                    System.out.println("Your balance is ₹" + bal);
+                    break;
+                case 2:
+                    int dep = 0;
+                    System.out.println("Enter your deposit amount");
+                    dep = sc.nextInt();
+                    if (dep > 0) {
+                        bal = dep + bal;
+                        System.out.println("Your deposit amount is " + dep + "\n" + "Total balance: " + bal);
+                        break;
+                    } else {
+                        System.out.println("Invalid deposit amount");
+                    }
+                    break;
+                case 3:
+                    int withdraw = 0;
+                    System.out.println("Enter your Withdraw amount");
+                    withdraw = sc.nextInt();
+                    if (withdraw > 0 ){
+                        System.out.println("your withdrawl amount is " + withdraw +"\n" +"your remaining balance is "+ (bal - withdraw));
+                    }
+
+
+            }
+
+
+        }
 
     }
 }
