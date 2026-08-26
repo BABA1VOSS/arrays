9eabdea6fd5e0c6482962dd709889e78e891b3b2
RJ6776-AKJ <rj6776jha@gmail.com>
2026-08-26 20:58:04 +0530

    for removing vertical lines

--- src/switchCaseProblemNumber6.java
+++ src/switchCaseProblemNumber6.java
@@ -1,5 +1,14 @@
+//------ BANK MENU ------
+//
+//1. Check Balance
+//2. Deposit Money
+//3. Withdraw Money
+//4. Change PIN
+//5. Exit
+//
+//Enter your choice:
 public class switchCaseProblemNumber6 {
-    static void main() {
+    public static void main() {
 
     }
 }
